52c26f3ead82194e96aa8c2a6dfc6624dae37009
palak <bhatnagarpalak14@gmail.com>
2025-08-18 19:51:26 +0530

    commit in sequence

---
+++ class_code/dict.py
@@ -0,0 +1,38 @@
+student={
+    "name":"shivi",
+    "address":"meerut",
+    "roll_no": 25,
+    "teachers":["jiya","diya","akshara"],
+    "subject":{
+        "jiya":"maths",
+        "diya":"history",
+        "akshara":"hindi"
+    },
+}
+
+print(student["roll_no"])
+    # a=student["teachers"]
+    # print(a[1])
+
+print(student["teachers"][1]) 
+
+
+print(student["subject"].items())
+for teacher,sub in student["subject"].items():
+    if teacher=="diya":
+        print(f"{teacher}-{sub}")
+
+
+print(student["subject"]["diya"])
+print(student["subject"].get("jiya"))
+# o=student["subject"].keys()
+# print(o)
+
+for sub in student["subject"].items():
+    if sub[0]=="diya":
+     print(f"{sub}")
+     continue
+
+
+
+
--- my_practice/function.py
+++
@@ -1,6 +0,0 @@
-def goodday(name,end):
-    print("good day " + name , end)
-    return "bye"
-a=goodday("palak","thankeww")
-print(a)
-
